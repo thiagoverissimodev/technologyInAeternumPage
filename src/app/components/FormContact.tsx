7bac69f28c61f5fe385b8565d30803dbd165741a
Thiago Verissimo <thiagovsc96@gmail.com>
2023-08-17 09:38:42 -0400

    initial

---
+++ src/app/components/FormContact.tsx
@@ -0,0 +1,92 @@
+/*
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ],
+  }
+  ```
+*/
+
+export default function FormContact() {
+  return (
+    <form>
+      <div className="space-y-12">
+        <div className="border-b border-gray-900/10 pb-12">
+          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 pt-8">
+            <div className="sm:col-span-full">
+              <div className="mt-2">
+                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 w-full">
+                  <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    autoComplete="name"
+                    className="block flex-1 border-0 bg-transparent p-3 lg:p-6 pl-1 text-gray-100 placeholder:text-gray-400 placeholder:lg:text-3xl placeholder:p-3 focus:ring-0 focus:p-4 focus:lg:text-3xl sm:leading-6 w-full"
+                    placeholder="Nome"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="sm:col-span-full">
+              <div className="mt-2">
+                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 w-full">
+                  <input
+                    type="text"
+                    name="email"
+                    id="email"
+                    autoComplete="email"
+                    className="block flex-1 border-0 bg-transparent p-3 lg:p-6 pl-1 text-gray-100 placeholder:text-gray-400 placeholder:lg:text-3xl placeholder:p-3 focus:ring-0 focus:p-4 focus:lg:text-3xl sm:leading-6 w-full"
+                    placeholder="E-mail"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="sm:col-span-full">
+              <div className="mt-2">
+                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 w-full">
+                  <input
+                    type="text"
+                    name="phone"
+                    id="phone"
+                    autoComplete="phone"
+                    className="block flex-1 border-0 bg-transparent p-3 lg:p-6 pl-1 text-gray-100 placeholder:text-gray-400 placeholder:lg:text-3xl placeholder:p-3 focus:ring-0 focus:p-4 focus:lg:text-3xl sm:leading-6 w-full"
+                    placeholder="Telefone (WhatsApp)"
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className="col-span-full">
+              <div className="mt-2">
+                <textarea
+                  id="message"
+                  name="message"
+                  rows={3}
+                  className="block w-full rounded-md border-0 p-3 text-gray-400 shadow-sm ring-1 ring-inset ring-gray-300 bg-transparent placeholder:p-3  placeholder:text-gray-400 placeholder:lg:text-3xl focus:p-3 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:leading-6"
+                  placeholder={'Mensagem'}
+                />
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+
+      <div className="mt-6 flex items-center justify-start gap-x-6">
+        <button
+          type="submit"
+          className="bg-green-600 rounded-xl block text-white lg:text-2xl uppercase px-10 sm:w-[35vw] lg:w-[30vw] py-5 sm:mt-10 mt-4 hover:bg-green-700 transition"
+        >
+          Fazer orçamento
+        </button>
+      </div>
+    </form>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,227 @@
-import Image from 'next/image'
+"use client";
+import Image from "next/image";
+import Header from "./components/Header";
+import SquigglyLines from "./components/SquigglyLines";
+import Link from "next/link";
+import {
+  DeviceMobile,
+  EnvelopeSimple,
+  Gauge,
+  Phone,
+  Planet,
+  ShieldCheck,
+  WhatsappLogo,
+} from "@phosphor-icons/react";
+import FormContact from "./components/FormContact";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
+    <div className="flex max-w-[1400] mx-auto flex-col items-center justify-center py-2 min-h-screen">
+      <Header />
+      <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 sm:mt-20 mt-20 background-gradient">
+        <a
+          href="https://api.whatsapp.com/send/?phone=5565999103791&text=Ol%C3%A1!%20Estou%20interessado(a)%20em%construir%20um%site!"
+          target="_blank"
+          rel="noreferrer"
+          className="border border-gray-700 rounded-lg py-4 px-6 text-gray-400 text-s=mm mb-5 transition duration-300 ease-in-out"
+        >
+          Design inovador, funcionalidades impactantes e{" "}
+          <span className="text-green-600">altas conversões</span>
+        </a>
+        <h1 className="mx-auto max-w-4xl font-display text-5xl font-bold tracking-normal text-gray-300 sm:text-7xl">
+          Ganhe mais na Web com{" "}
+          <span className="relative whitespace-nowrap text-green-600">
+            <SquigglyLines />
+            <span className="relative">Páginas</span>
+          </span>{" "}
+          que convertem.
+        </h1>
+        <h2 className="mx-auto mt-12 max-w-xl text-lg sm:text-gray-400  text-gray-500 leading-7">
+          Transformamos negócios com páginas de alto nível e com um retorno
+          acima do investido.
+        </h2>
+        <Link
+          className="bg-green-600 rounded-xl block text-white lg:text-2xl uppercase px-10 sm:w-[35vw] lg:w-[30vw] py-5 sm:mt-10 mt-4 hover:bg-green-700 transition"
+          href="https://api.whatsapp.com/send/?phone=5565999103791&text=Ol%C3%A1!%20Estou%20interessado(a)%20em20%construir%20um%20site!"
+        >
+          Comece seu site hoje
+        </Link>
+        <div className="flex flex-col justify-center items-center my-12 py-10">
+          <div className="pt-9 flex flex-col">
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+              alt="Generated photo of a room with roomGPT.io"
+              width={820}
+              height={462}
+              src="/capa-digit-teclado-cta.png"
+              className="w-[75vw] object-cover h-760 rounded-2xl sm:mt-0 mt-2 shadow-green-main"
             />
-          </a>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+        <div className="flex w-full flex-col sm:mt-10 mt-6">
+          <h1 className="mx-auto max-w-7xl font-display text-4xl font-bold pb-12 tracking-normal text-gray-300 md:text-6xl">
+            Mais clientes, mais visibilidade, mais confiança{" "}
+            <span className="text-green-600">mais negócios todos os dias.</span>
+          </h1>
+          <div className="flex flex-col space-y-10 mt-6 mb-16">
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 sm:space-x-6 sm:px-8">
+              <div className="col-span-1 sm:p-12 p-2">
+                <h3 className="mb-1 font-medium text-4xl text-start p-6">
+                  A Technology in Aeternum
+                </h3>
+                <div className="p-7">
+                  <p className="sm:text-2xl text-xl py-2 text-left">
+                    A Technology in Aeternum surge no mercado ao enxergar que
+                    muitas páginas criadas, mesmo nos dias atuais, não são
+                    profissionais, são lentas e não convertem.
+                  </p>
+                  <p className="sm:text-2xl text-xl py-2 text-left">
+                    A empresa veio para trazer profissionalismo, velocidade,
+                    credibilidade, e claro, muitas vendas.
+                  </p>
+                  <p className="sm:text-2xl text-xl py-2 text-left">
+                    A empresa foi fundada pelo Thiago Verissimo, professor
+                    universitário, analista e desenvolvedor de sistemas, com
+                    foco em construir páginas modernas e decisivas que aumentem
+                    a visibilidade e conversão dos seus clientes
+                  </p>
+                  <Link
+                    className="bg-green-600 block rounded-xl text-white lg:text-2xl uppercase px-10 sm:w-[35vw] lg:w-[30vw] py-5 sm:mt-12 mt-4 hover:bg-green-700 transition"
+                    href="https://api.whatsapp.com/send/?phone=5565999103791&text=Ol%C3%A1!%20Estou%20interessado(a)%20em20%construir%20um%20site!"
+                  >
+                    Comece seu site hoje
+                  </Link>
+                </div>
+              </div>
+              <div className="col-span-1 sm:mt-0 mt-8 sm:p-12 p-2">
+                <div className="image-container">
+                  <div className="bg-gradient-custom-radial"></div>
+                  <Image
+                    alt="Macbook com um template na tela"
+                    width={500}
+                    height={500}
+                    src="/ae-ft-perfil-without-dot.png"
+                    className="w-full block object-cover lg:h-[690px] rounded-2xl sm:mt-0 mt-2"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex w-full flex-col sm:mt-10 mt-6">
+          <h1 className="mx-auto max-w-7xl font-display text-5xl font-bold pb-12 tracking-normal text-gray-300 sm:text-7xl">
+            Um site profissional muda o{" "}
+            <span className="text-green-600">rumo do seu negócio.</span>
+          </h1>
+          <div className="grid grid-cols-1 mx-7 md:grid-cols-2 gap-6 sm:p-12">
+            <figure className="border border-green-900 rounded-xl sm:p-12 p-2">
+              <div className="flex items-center justify-center">
+                <Gauge
+                  size={96}
+                  className="bg-gradient-to-r from-green-500 to-green-800 rounded"
+                />
+              </div>
+              <div className="pt-6 t space-y-4">
+                <h1 className="sm:text-3xl font-medium text-2xl pt-6">
+                  Sites Otimizados
+                </h1>
+                <p className="sm:text-2xl py-4">
+                  Tenha um site que carrega rapidamente e aumente suas vendas e
+                  conversões de forma eficaz.
+                </p>
+              </div>
+            </figure>
+            <figure className="border border-green-900 rounded-xl sm:p-12 p-2">
+              <div className="flex items-center justify-center">
+                <DeviceMobile
+                  size={95}
+                  className="bg-gradient-to-r from-green-500 to-green-800 rounded"
+                />
+              </div>
+              <div className="pt-6 t space-y-4">
+                <h1 className="sm:text-3xl font-medium text-2xl pt-6">
+                  Design Responsivo
+                </h1>
+                <p className="sm:text-2xl py-4">
+                  Seu site acessível em todos os dispositivos móveis. A maioria
+                  dos seus clientes estão navegando pelo celular e o seu site
+                  precisa estar preparado. Não perca mais vendas.
+                </p>
+              </div>
+            </figure>
+            <figure className="border border-green-900 rounded-xl sm:p-12 p-2">
+              <div className="flex items-center justify-center">
+                <ShieldCheck
+                  size={96}
+                  className="bg-gradient-to-r from-green-500 to-green-800 rounded"
+                />
+              </div>
+              <div className="pt-6 t space-y-4">
+                <h1 className="sm:text-3xl font-medium text-2xl pt-6">
+                  Selo de Segurança
+                </h1>
+                <p className="sm:text-2xl py-4">
+                  Certificado SSL instalado para garantir mais segurança para
+                  você e para os seus cliente ao acessarem o site.
+                </p>
+              </div>
+            </figure>
+            <figure className="border border-green-900 rounded-xl sm:p-12 p-2">
+              <div className="flex items-center justify-center">
+                <Planet
+                  size={96}
+                  className="bg-gradient-to-r from-green-500 to-green-800 rounded"
+                />
+              </div>
+              <div className="pt-6 t space-y-4">
+                <h1 className="sm:text-3xl font-medium text-2xl pt-6">
+                  Mais alcance
+                </h1>
+                <p className="sm:text-2xl py-4">
+                  Uma página web irá garantir sua presença online, alcançar
+                  audiências internacionais e explorar oportunidades de negócios
+                  24 horas, 7 dias por semana!
+                </p>
+              </div>
+            </figure>
+          </div>
+        </div>
+        <div className="flex w-full flex-col sm:mt-10 mt-6">
+          <p className="py-4 px-6 text-green-400 text-2xl mb-5 transition duration-300 ease-in-out">
+            Vamos conversar sobre o seu site?
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+          <h1 className="mx-auto max-w-7xl font-display text-5xl font-bold pb-12 tracking-normal text-gray-300 sm:text-7xl">
+            Solicite seu orçamento
+            <span className="text-green-600">{" "}agora mesmo!</span>
+          </h1>
+          <div className="grid grid-cols-1 mx-7 md:grid-cols-2 gap-6 sm:p-12">
+            <div className="flex flex-col lg:items-start justify-around p-8">
+              <div className="flex flex-col md:flex-row py-8 items-center justify-center md:justify-between">
+                <WhatsappLogo size={96} className="bg-green-500 rounded py-4 min-w-[96px]"/>
+                <div className="flex flex-col items-center lg:items-start justify-center p-4">
+                  <span className="md:text-3xl">Chamar no WhatsApp</span>
+                </div>
+              </div>
+              <div className="flex flex-col md:flex-row py-8 items-center justify-center md:justify-between">
+                <Phone size={96} className="bg-green-500 rounded py-4 min-w-[96px]"/>
+                <div className="flex flex-col items-center lg:items-start justify-center p-4">
+                  <span className="text-sm md:text-2xl lg:text-start">Ligue já</span>
+                  <span className="text-sm md:text-3xl">(65) 99910-3791</span>
+                </div>
+              </div>
+              <div className="flex flex-col md:flex-row py-8 items-center justify-center md:justify-between">
+                <EnvelopeSimple size={96} className="bg-green-500 rounded py-4 min-w-[96px]"/>
+                <div className="flex flex-col items-center lg:items-start justify-center p-4">
+                  <span className="text-sm md:text-2xl text-start">Entre em contato</span>
+                  <span className="text-sm md:text-1xl break-words">contato@technologyinaeternum.com.br</span>
+                </div>
+              </div>
+            </div>
+            <FormContact />
+          </div>
+        </div>
+      </main>
+        <Footer />
+    </div>
+  );
 }
